c5d70f41fcd6a596bc9c3fa7b7e4b508487fb89f
John Whitton <john@johnwhitton.com>
2021-04-14 14:41:38 -0700

    Upgrade for latest XCM

--- xcm-simulator/src/traits.rs
+++ xcm-simulator/src/traits.rs
@@ -11,17 +11,17 @@ pub trait UmpMsgHandler {
 }
 
 pub trait HrmpMsgHandler {
-	fn handle_hrmp_msg(from: u32, msg: VersionedXcm) -> Result<(), ()>;
+	fn handle_hrmp_msg(from: u32, msg: VersionedXcm<()>) -> Result<(), ()>;
 }
 
 pub trait DmpMsgHandler {
-	fn handle_dmp_msg(msg: Xcm) -> XcmResult;
+	fn handle_dmp_msg(msg: Xcm<()>) -> XcmResult;
 }
 
 pub trait XcmRelay {
 	fn send_ump_msg(from: u32, msg: Vec<u8>) -> Result<(), ()>;
-	fn send_hrmp_msg(from: u32, to: u32, msg: VersionedXcm) -> Result<(), ()>;
-	fn send_dmp_msg(to: u32, msg: Xcm) -> XcmResult;
+	fn send_hrmp_msg(from: u32, to: u32, msg: VersionedXcm<()>) -> Result<(), ()>;
+	fn send_dmp_msg(to: u32, msg: Xcm<()>) -> XcmResult;
 }
 
 pub trait GetParaId {
