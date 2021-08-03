0e46d3ee19b0cf8f172207014994e04296ae64ba
Shaun Wang <spxwang@gmail.com>
2021-08-03 17:31:10 +1200

    Remove unused deps.

--- xcm-emulator/example/src/lib.rs
+++ xcm-emulator/example/src/lib.rs
@@ -3,14 +3,6 @@ use sp_runtime::AccountId32;
 
 use xcm_emulator::{decl_test_network, decl_test_parachain, decl_test_relay_chain};
 
-decl_test_parachain! {
-	pub struct Statemine {
-		Runtime = statemine_runtime::Runtime,
-		Origin = statemine_runtime::Origin,
-		new_ext = statemine_ext(),
-	}
-}
-
 decl_test_relay_chain! {
 	pub struct Kusama {
 		Runtime = kusama_runtime::Runtime,
@@ -50,38 +42,12 @@ decl_test_network! {
 			(1, YayoiPumpkin),
 			(2, YayoiMushroom),
 			(3, YayoiOctopus),
-			(1000, Statemine),
 		],
 	}
 }
 
 pub const ALICE: AccountId32 = AccountId32::new([0u8; 32]);
 pub const INITIAL_BALANCE: u128 = 1_000_000_000_000;
-
-pub fn statemine_ext() -> sp_io::TestExternalities {
-	use statemine_runtime::{Runtime, System};
-
-	let mut t = frame_system::GenesisConfig::default()
-		.build_storage::<Runtime>()
-		.unwrap();
-
-	let parachain_info_config = parachain_info::GenesisConfig {
-		parachain_id: 1_000.into(),
-	};
-
-	<parachain_info::GenesisConfig as GenesisBuild<Runtime, _>>::assimilate_storage(&parachain_info_config, &mut t)
-		.unwrap();
-
-	pallet_balances::GenesisConfig::<Runtime> {
-		balances: vec![(ALICE, INITIAL_BALANCE)],
-	}
-	.assimilate_storage(&mut t)
-	.unwrap();
-
-	let mut ext = sp_io::TestExternalities::new(t);
-	ext.execute_with(|| System::set_block_number(1));
-	ext
-}
 
 pub fn yayoi_ext(para_id: u32) -> sp_io::TestExternalities {
 	use yayoi::{Runtime, System};
@@ -125,11 +91,6 @@ fn default_parachains_host_configuration(
 		max_upward_queue_count: 8,
 		max_upward_queue_size: 1024 * 1024,
 		max_downward_message_size: 1024,
-		// this is approximatelly 4ms.
-		//
-		// Same as `4 * frame_support::weights::WEIGHT_PER_MILLIS`. We don't bother with
-		// an import since that's a made up number and should be replaced with a constant
-		// obtained by benchmarking anyway.
 		ump_service_total_weight: 4 * 1_000_000_000,
 		max_upward_message_size: 1024 * 1024,
 		max_upward_message_num_per_candidate: 5,
