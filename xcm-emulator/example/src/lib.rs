c5350f6158265e5d79b22f3be3b13ad2358afb15
Shaun Wang <spxwang@gmail.com>
2021-08-03 20:33:47 +1200

    XCM emulator (#12)
    * Handle ump and dmp.
    
    * Remove unused deps.
    
    * fmt
    
    * fix
    
    * XCM emulator.
    
    * fmt
    
    * Refactor workspace.
    
    * Yayoi runtime.
    
    * Refactor messaging traits.
    
    * Make clippy happy.
    
    * Horizontal messages handling.
    
    * fmt
    
    * Update messages process.
    
    * More unit tests.
    
    * Make clippy happy.
    
    * Remove unused deps.
    
    * Fix typo.

---
+++ xcm-emulator/example/src/lib.rs
@@ -0,0 +1,302 @@
+use frame_support::traits::GenesisBuild;
+use sp_runtime::AccountId32;
+
+use xcm_emulator::{decl_test_network, decl_test_parachain, decl_test_relay_chain};
+
+decl_test_relay_chain! {
+	pub struct Kusama {
+		Runtime = kusama_runtime::Runtime,
+		XcmConfig = kusama_runtime::XcmConfig,
+		new_ext = kusama_ext(),
+	}
+}
+
+decl_test_parachain! {
+	pub struct YayoiPumpkin {
+		Runtime = yayoi::Runtime,
+		Origin = yayoi::Origin,
+		new_ext = yayoi_ext(1),
+	}
+}
+
+decl_test_parachain! {
+	pub struct YayoiMushroom {
+		Runtime = yayoi::Runtime,
+		Origin = yayoi::Origin,
+		new_ext = yayoi_ext(2),
+	}
+}
+
+decl_test_parachain! {
+	pub struct YayoiOctopus {
+		Runtime = yayoi::Runtime,
+		Origin = yayoi::Origin,
+		new_ext = yayoi_ext(3),
+	}
+}
+
+decl_test_network! {
+	pub struct Network {
+		relay_chain = Kusama,
+		parachains = vec![
+			(1, YayoiPumpkin),
+			(2, YayoiMushroom),
+			(3, YayoiOctopus),
+		],
+	}
+}
+
+pub const ALICE: AccountId32 = AccountId32::new([0u8; 32]);
+pub const INITIAL_BALANCE: u128 = 1_000_000_000_000;
+
+pub fn yayoi_ext(para_id: u32) -> sp_io::TestExternalities {
+	use yayoi::{Runtime, System};
+
+	let mut t = frame_system::GenesisConfig::default()
+		.build_storage::<Runtime>()
+		.unwrap();
+
+	let parachain_info_config = parachain_info::GenesisConfig {
+		parachain_id: para_id.into(),
+	};
+
+	<parachain_info::GenesisConfig as GenesisBuild<Runtime, _>>::assimilate_storage(&parachain_info_config, &mut t)
+		.unwrap();
+
+	pallet_balances::GenesisConfig::<Runtime> {
+		balances: vec![(ALICE, INITIAL_BALANCE)],
+	}
+	.assimilate_storage(&mut t)
+	.unwrap();
+
+	let mut ext = sp_io::TestExternalities::new(t);
+	ext.execute_with(|| System::set_block_number(1));
+	ext
+}
+
+fn default_parachains_host_configuration(
+) -> polkadot_runtime_parachains::configuration::HostConfiguration<polkadot_primitives::v1::BlockNumber> {
+	use polkadot_primitives::v1::{MAX_CODE_SIZE, MAX_POV_SIZE};
+
+	polkadot_runtime_parachains::configuration::HostConfiguration {
+		validation_upgrade_frequency: 1u32,
+		validation_upgrade_delay: 1,
+		code_retention_period: 1200,
+		max_code_size: MAX_CODE_SIZE,
+		max_pov_size: MAX_POV_SIZE,
+		max_head_data_size: 32 * 1024,
+		group_rotation_frequency: 20,
+		chain_availability_period: 4,
+		thread_availability_period: 4,
+		max_upward_queue_count: 8,
+		max_upward_queue_size: 1024 * 1024,
+		max_downward_message_size: 1024,
+		ump_service_total_weight: 4 * 1_000_000_000,
+		max_upward_message_size: 1024 * 1024,
+		max_upward_message_num_per_candidate: 5,
+		hrmp_open_request_ttl: 5,
+		hrmp_sender_deposit: 0,
+		hrmp_recipient_deposit: 0,
+		hrmp_channel_max_capacity: 8,
+		hrmp_channel_max_total_size: 8 * 1024,
+		hrmp_max_parachain_inbound_channels: 4,
+		hrmp_max_parathread_inbound_channels: 4,
+		hrmp_channel_max_message_size: 1024 * 1024,
+		hrmp_max_parachain_outbound_channels: 4,
+		hrmp_max_parathread_outbound_channels: 4,
+		hrmp_max_message_num_per_candidate: 5,
+		dispute_period: 6,
+		no_show_slots: 2,
+		n_delay_tranches: 25,
+		needed_approvals: 2,
+		relay_vrf_modulo_samples: 2,
+		zeroth_delay_tranche_width: 0,
+		..Default::default()
+	}
+}
+
+pub fn kusama_ext() -> sp_io::TestExternalities {
+	use kusama_runtime::{Runtime, System};
+
+	let mut t = frame_system::GenesisConfig::default()
+		.build_storage::<Runtime>()
+		.unwrap();
+
+	pallet_balances::GenesisConfig::<Runtime> {
+		balances: vec![(ALICE, INITIAL_BALANCE)],
+	}
+	.assimilate_storage(&mut t)
+	.unwrap();
+
+	polkadot_runtime_parachains::configuration::GenesisConfig::<Runtime> {
+		config: default_parachains_host_configuration(),
+	}
+	.assimilate_storage(&mut t)
+	.unwrap();
+
+	let mut ext = sp_io::TestExternalities::new(t);
+	ext.execute_with(|| System::set_block_number(1));
+	ext
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use codec::Encode;
+
+	use cumulus_primitives_core::ParaId;
+	use frame_support::{assert_ok, traits::Currency};
+	use sp_runtime::traits::AccountIdConversion;
+	use xcm::v0::{
+		Junction::{Parachain, Parent},
+		MultiAsset::*,
+		MultiLocation::*,
+		Order::BuyExecution,
+		OriginKind,
+		Xcm::*,
+	};
+	use xcm_emulator::TestExt;
+
+	#[test]
+	fn dmp() {
+		Network::reset();
+
+		let remark = yayoi::Call::System(frame_system::Call::<yayoi::Runtime>::remark_with_event(
+			"Hello from Kusama!".as_bytes().to_vec(),
+		));
+		Kusama::execute_with(|| {
+			assert_ok!(kusama_runtime::XcmPallet::send_xcm(
+				Null,
+				Parachain(1).into(),
+				Transact {
+					origin_type: OriginKind::SovereignAccount,
+					require_weight_at_most: 10_000_000,
+					call: remark.encode().into(),
+				}
+			));
+		});
+
+		YayoiPumpkin::execute_with(|| {
+			use yayoi::{Event, System};
+			assert!(System::events()
+				.iter()
+				.any(|r| matches!(r.event, Event::System(frame_system::Event::Remarked(_, _)))));
+		});
+	}
+
+	#[test]
+	fn ump() {
+		Network::reset();
+
+		Kusama::execute_with(|| {
+			let _ = kusama_runtime::Balances::deposit_creating(&ParaId::from(1).into_account(), 1_000_000_000_000);
+		});
+
+		let remark = kusama_runtime::Call::System(frame_system::Call::<kusama_runtime::Runtime>::remark_with_event(
+			"Hello from Pumpkin!".as_bytes().to_vec(),
+		));
+		YayoiPumpkin::execute_with(|| {
+			assert_ok!(yayoi::PolkadotXcm::send_xcm(
+				Null,
+				Parent.into(),
+				WithdrawAsset {
+					assets: vec![ConcreteFungible {
+						id: Null,
+						amount: 1_000_000_000_000
+					}],
+					effects: vec![BuyExecution {
+						fees: All,
+						weight: 10_000_000,
+						debt: 10_000_000,
+						halt_on_error: true,
+						xcm: vec![Transact {
+							origin_type: OriginKind::SovereignAccount,
+							require_weight_at_most: 1_000_000_000,
+							call: remark.encode().into(),
+						}],
+					}]
+				}
+			));
+		});
+
+		Kusama::execute_with(|| {
+			use kusama_runtime::{Event, System};
+			assert!(System::events()
+				.iter()
+				.any(|r| matches!(r.event, Event::System(frame_system::Event::Remarked(_, _)))));
+		});
+	}
+
+	#[test]
+	fn xcmp() {
+		Network::reset();
+
+		let remark = yayoi::Call::System(frame_system::Call::<yayoi::Runtime>::remark_with_event(
+			"Hello from Pumpkin!".as_bytes().to_vec(),
+		));
+		YayoiPumpkin::execute_with(|| {
+			assert_ok!(yayoi::PolkadotXcm::send_xcm(
+				Null,
+				X2(Parent, Parachain(2)),
+				Transact {
+					origin_type: OriginKind::SovereignAccount,
+					require_weight_at_most: 10_000_000,
+					call: remark.encode().into(),
+				},
+			));
+		});
+
+		YayoiMushroom::execute_with(|| {
+			use yayoi::{Event, System};
+			assert!(System::events()
+				.iter()
+				.any(|r| matches!(r.event, Event::System(frame_system::Event::Remarked(_, _)))));
+		});
+	}
+
+	#[test]
+	fn xcmp_through_a_parachain() {
+		use yayoi::{Call, PolkadotXcm, Runtime};
+
+		Network::reset();
+
+		// The message goes through: Pumpkin --> Mushroom --> Octopus
+		let remark = Call::System(frame_system::Call::<Runtime>::remark_with_event(
+			"Hello from Pumpkin!".as_bytes().to_vec(),
+		));
+		let send_xcm_to_octopus = Call::PolkadotXcm(pallet_xcm::Call::<Runtime>::send(
+			X2(Parent, Parachain(3)),
+			Transact {
+				origin_type: OriginKind::SovereignAccount,
+				require_weight_at_most: 10_000_000,
+				call: remark.encode().into(),
+			},
+		));
+		YayoiPumpkin::execute_with(|| {
+			assert_ok!(PolkadotXcm::send_xcm(
+				Null,
+				X2(Parent, Parachain(2)),
+				Transact {
+					origin_type: OriginKind::SovereignAccount,
+					require_weight_at_most: 100_000_000,
+					call: send_xcm_to_octopus.encode().into(),
+				},
+			));
+		});
+
+		YayoiMushroom::execute_with(|| {
+			use yayoi::{Event, System};
+			assert!(System::events()
+				.iter()
+				.any(|r| matches!(r.event, Event::PolkadotXcm(pallet_xcm::Event::Sent(_, _, _)))));
+		});
+
+		YayoiOctopus::execute_with(|| {
+			use yayoi::{Event, System};
+			// execution would fail, but good enough to check if the message is received
+			assert!(System::events()
+				.iter()
+				.any(|r| matches!(r.event, Event::XcmpQueue(cumulus_pallet_xcmp_queue::Event::Fail(_, _)))));
+		});
+	}
+}
---
+++ xcm-emulator/yayoi/src/lib.rs
@@ -0,0 +1,211 @@
+use frame_support::{
+	construct_runtime, parameter_types,
+	traits::All,
+	weights::{constants::WEIGHT_PER_SECOND, Weight},
+};
+use frame_system::EnsureRoot;
+use sp_core::H256;
+use sp_runtime::{
+	testing::Header,
+	traits::{Convert, IdentityLookup},
+	AccountId32,
+};
+
+use pallet_xcm::XcmPassthrough;
+use polkadot_parachain::primitives::Sibling;
+use xcm::v0::{
+	Junction::{self, Parachain, Parent},
+	MultiAsset,
+	MultiLocation::{self, X1},
+	NetworkId, Xcm,
+};
+use xcm_builder::{
+	AccountId32Aliases, AllowUnpaidExecutionFrom, EnsureXcmOrigin, FixedWeightBounds, LocationInverter,
+	ParentIsDefault, RelayChainAsNative, SiblingParachainAsNative, SiblingParachainConvertsVia,
+	SignedAccountId32AsNative, SignedToAccountId32, SovereignSignedViaLocation,
+};
+use xcm_executor::{Config, XcmExecutor};
+
+pub type AccountId = AccountId32;
+pub type Balance = u128;
+pub type Amount = i128;
+
+parameter_types! {
+	pub const BlockHashCount: u64 = 250;
+}
+
+impl frame_system::Config for Runtime {
+	type Origin = Origin;
+	type Call = Call;
+	type Index = u64;
+	type BlockNumber = u64;
+	type Hash = H256;
+	type Hashing = ::sp_runtime::traits::BlakeTwo256;
+	type AccountId = AccountId;
+	type Lookup = IdentityLookup<Self::AccountId>;
+	type Header = Header;
+	type Event = Event;
+	type BlockHashCount = BlockHashCount;
+	type BlockWeights = ();
+	type BlockLength = ();
+	type Version = ();
+	type PalletInfo = PalletInfo;
+	type AccountData = pallet_balances::AccountData<Balance>;
+	type OnNewAccount = ();
+	type OnKilledAccount = ();
+	type DbWeight = ();
+	type BaseCallFilter = ();
+	type SystemWeightInfo = ();
+	type SS58Prefix = ();
+	type OnSetCode = cumulus_pallet_parachain_system::ParachainSetCode<Self>;
+}
+
+parameter_types! {
+	pub ExistentialDeposit: Balance = 1;
+	pub const MaxLocks: u32 = 50;
+	pub const MaxReserves: u32 = 50;
+}
+
+impl pallet_balances::Config for Runtime {
+	type MaxLocks = MaxLocks;
+	type Balance = Balance;
+	type Event = Event;
+	type DustRemoval = ();
+	type ExistentialDeposit = ExistentialDeposit;
+	type AccountStore = System;
+	type WeightInfo = ();
+	type MaxReserves = MaxReserves;
+	type ReserveIdentifier = [u8; 8];
+}
+
+impl parachain_info::Config for Runtime {}
+
+parameter_types! {
+	pub const RelayLocation: MultiLocation = MultiLocation::X1(Parent);
+	pub const RelayNetwork: NetworkId = NetworkId::Kusama;
+	pub RelayChainOrigin: Origin = cumulus_pallet_xcm::Origin::Relay.into();
+	pub Ancestry: MultiLocation = Parachain(ParachainInfo::parachain_id().into()).into();
+}
+
+pub type LocationToAccountId = (
+	ParentIsDefault<AccountId>,
+	SiblingParachainConvertsVia<Sibling, AccountId>,
+	AccountId32Aliases<RelayNetwork, AccountId>,
+);
+
+pub type XcmOriginToCallOrigin = (
+	SovereignSignedViaLocation<LocationToAccountId, Origin>,
+	RelayChainAsNative<RelayChainOrigin, Origin>,
+	SiblingParachainAsNative<cumulus_pallet_xcm::Origin, Origin>,
+	SignedAccountId32AsNative<RelayNetwork, Origin>,
+	XcmPassthrough<Origin>,
+);
+
+parameter_types! {
+	pub const UnitWeightCost: Weight = 10;
+}
+
+pub type LocalAssetTransactor = ();
+
+/// The means for routing XCM messages which are not for local execution into
+/// the right message queues.
+pub type XcmRouter = (
+	// Two routers - use UMP to communicate with the relay chain:
+	cumulus_primitives_utility::ParentAsUmp<ParachainSystem>,
+	// ..and XCMP to communicate with the sibling chains.
+	XcmpQueue,
+);
+
+pub type Barrier = AllowUnpaidExecutionFrom<All<MultiLocation>>;
+
+pub struct XcmConfig;
+impl Config for XcmConfig {
+	type Call = Call;
+	type XcmSender = XcmRouter;
+	type AssetTransactor = LocalAssetTransactor;
+	type OriginConverter = XcmOriginToCallOrigin;
+	type IsReserve = ();
+	type IsTeleporter = ();
+	type LocationInverter = LocationInverter<Ancestry>;
+	type Barrier = Barrier;
+	type Weigher = FixedWeightBounds<UnitWeightCost, Call>;
+	type Trader = ();
+	type ResponseHandler = ();
+}
+
+parameter_types! {
+	pub const ReservedXcmpWeight: Weight = WEIGHT_PER_SECOND / 4;
+	pub const ReservedDmpWeight: Weight = WEIGHT_PER_SECOND / 4;
+}
+
+impl cumulus_pallet_parachain_system::Config for Runtime {
+	type Event = Event;
+	type OnValidationData = ();
+	type SelfParaId = ParachainInfo;
+	type DmpMessageHandler = DmpQueue;
+	type ReservedDmpWeight = ReservedDmpWeight;
+	type OutboundXcmpMessageSource = XcmpQueue;
+	type XcmpMessageHandler = XcmpQueue;
+	type ReservedXcmpWeight = ReservedXcmpWeight;
+}
+
+impl cumulus_pallet_xcmp_queue::Config for Runtime {
+	type Event = Event;
+	type XcmExecutor = XcmExecutor<XcmConfig>;
+	type ChannelInfo = ParachainSystem;
+}
+
+impl cumulus_pallet_dmp_queue::Config for Runtime {
+	type Event = Event;
+	type XcmExecutor = XcmExecutor<XcmConfig>;
+	type ExecuteOverweightOrigin = EnsureRoot<AccountId>;
+}
+
+impl cumulus_pallet_xcm::Config for Runtime {
+	type Event = Event;
+	type XcmExecutor = XcmExecutor<XcmConfig>;
+}
+
+pub type LocalOriginToLocation = SignedToAccountId32<Origin, AccountId, RelayNetwork>;
+
+impl pallet_xcm::Config for Runtime {
+	type Event = Event;
+	type SendXcmOrigin = EnsureXcmOrigin<Origin, LocalOriginToLocation>;
+	type XcmRouter = XcmRouter;
+	type ExecuteXcmOrigin = EnsureXcmOrigin<Origin, LocalOriginToLocation>;
+	type XcmExecuteFilter = All<(MultiLocation, Xcm<Call>)>;
+	type XcmExecutor = XcmExecutor<XcmConfig>;
+	type XcmTeleportFilter = ();
+	type XcmReserveTransferFilter = All<(MultiLocation, Vec<MultiAsset>)>;
+	type Weigher = FixedWeightBounds<UnitWeightCost, Call>;
+}
+
+pub struct AccountIdToMultiLocation;
+impl Convert<AccountId, MultiLocation> for AccountIdToMultiLocation {
+	fn convert(account: AccountId) -> MultiLocation {
+		X1(Junction::AccountId32 {
+			network: NetworkId::Any,
+			id: account.into(),
+		})
+	}
+}
+
+type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Runtime>;
+type Block = frame_system::mocking::MockBlock<Runtime>;
+
+construct_runtime!(
+	pub enum Runtime where
+		Block = Block,
+		NodeBlock = Block,
+		UncheckedExtrinsic = UncheckedExtrinsic,
+	{
+		System: frame_system::{Pallet, Call, Storage, Config, Event<T>},
+		Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>},
+		ParachainSystem: cumulus_pallet_parachain_system::{Pallet, Call, Storage, Inherent, Config, Event<T>},
+		ParachainInfo: parachain_info::{Pallet, Storage, Config},
+		XcmpQueue: cumulus_pallet_xcmp_queue::{Pallet, Call, Storage, Event<T>},
+		DmpQueue: cumulus_pallet_dmp_queue::{Pallet, Call, Storage, Event<T>},
+		CumulusXcm: cumulus_pallet_xcm::{Pallet, Event<T>, Origin},
+		PolkadotXcm: pallet_xcm::{Pallet, Call, Event<T>, Origin},
+	}
+);
